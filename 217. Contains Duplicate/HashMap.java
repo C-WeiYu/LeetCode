02808c65b4a8d5693016a10c7db0753fe4a18512
Wei <55570378+C-WeiYu@users.noreply.github.com>
2024-07-28 17:53:25 +0800

    Update HashMap.java

--- 217. Contains Duplicate/HashMap.java
+++ 217. Contains Duplicate/HashMap.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        HashMap <Integer,Integer> hm = new HashMap<Integer,Integer> ();
+        Map <Integer,Integer> hm = new HashMap<Integer,Integer> ();
         for(int i = 0 ; i < nums.length ; i++){
             if(hm.containsKey(nums[i])){
                 return true;
