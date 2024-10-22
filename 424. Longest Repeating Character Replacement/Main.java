0da0e8b88000561290eb43d97cf0936a1fba04ac
Wei <55570378+C-WeiYu@users.noreply.github.com>
2024-10-22 16:54:33 +0800

    Create Main.java

---
+++ 424. Longest Repeating Character Replacement/Main.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int characterReplacement(String s, int k) {
+        int[] arr = new int[26];
+        int max = 0 ;
+        int ans = 0 ;
+        int l = 0 ;
+        for(int r = 0 ; r < s.length() ; r++ ){
+            arr[s.charAt(r) - 'A'] += 1 ;
+
+            max = Math.max(max , arr[s.charAt(r)-'A']);
+
+            if(r-l+1 - max > k){
+                arr[s.charAt(l)-'A'] -= 1 ;
+                l += 1 ;
+            }
+            ans = Math.max(ans, r-l+1);
+
+        }
+        return ans ;
+    }
+}
