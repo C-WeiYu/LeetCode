2213e74a139b550736da4aaf35dd573f2be3e194
Wei <55570378+C-WeiYu@users.noreply.github.com>
2025-02-18 17:11:22 +0800

    Update Main.java

--- 239. Sliding Window Maximum/Main.java
+++ 239. Sliding Window Maximum/Main.java
@@ -4,15 +4,19 @@ class Solution {
         Deque<Integer> monotonicQueue = new LinkedList<>();
         for(int i = 0; i < nums.length; i++){
             int num = nums[i];
+            // 加入的數字比當前queue中後n個數字都大時，把後面n個數直接刪掉
             while(!monotonicQueue.isEmpty() && num > monotonicQueue.peekLast()){
                 monotonicQueue.pollLast();
             }
             monotonicQueue.offerLast(num);
 
+            // 當目前加入的數的index已經大於等於k，表示window已滿
+            // 如果Queue中最左邊的數等於nums[i-k]，即表示這個數在目前window範圍以外，應此刪掉
             if(i >= k && monotonicQueue.peek() == nums[i-k]){
                 monotonicQueue.poll();
             }
-
+            
+            // 當window開始滿時，每次取最左邊(最大)的值
             if(i >= k-1){
                 res[i-k+1] = monotonicQueue.peek();
             }
