5ebb6c1820dea692ab5879d97c0d14662285c9e9
Wei <55570378+C-WeiYu@users.noreply.github.com>
2025-04-10 17:34:33 +0800

    Create Main.java

---
+++ 2999. Count the Number of Powerful Integers/Main.java
@@ -0,0 +1,31 @@
+class Solution {
+    public long numberOfPowerfulInt(long start, long finish, int limit, String s) {
+        String strStart = Long.toString(start - 1);
+        String strFinish = Long.toString(finish);
+        return calculate(strFinish, s, limit) - calculate(strStart, s, limit);
+    }
+    private long calculate(String x, String s, int limit){
+        if(x.length() < s.length()){
+            return 0;
+        }
+        if(x.length() == s.length()){
+            return x.compareTo(s) >= 0 ? 1 : 0;
+        }
+
+        String suffix = x.substring(x.length() - s.length());
+        long count = 0;
+        int preLen = x.length() - s.length();
+        for(int i = 0; i < preLen; i++){
+            int digit = x.charAt(i) - '0';
+            if(limit < digit){
+                count += (long) Math.pow(limit + 1, preLen - i);
+                return count;
+            }
+            count += (long) (digit) * (long) Math.pow(limit + 1, preLen - 1 - i);
+        }
+        if(suffix.compareTo(s) >= 0){
+            count++;
+        }
+        return count;
+    }
+}
